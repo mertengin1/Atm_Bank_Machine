e86023856de14b811c4406e0e1d8c2814b4b917f
Mert Engin <mert.engin@ogr.yesevi.edu.tr>
2025-02-17 13:53:08 +0300

    Update AtmMachine.c

--- AtmMachine.c
+++ AtmMachine.c
@@ -9,30 +9,30 @@ int main()
 
     bakiye = 5000;
 
-    printf("Hesabinizda %d TL bulunuyor.\n", bakiye);
-    printf("Hangi islemi yapmak istersiniz\n");
-    printf("yatirmak icin 'y' veya cekmek icin 'c' yaziniz:");
+    printf("Your bank account is %d $.\n", bakiye);
+    printf("Please choose a action to perform\n");
+    printf("For deposit 'y'\n for withdraw 'c':");
 
     scanf(" %c", &a);
 
     switch (a)
     {
     case 'y':
-        printf("Ne kadar yatirmak istersiniz:");
+        printf("How much you want to deposit?:");
         scanf("%d", &yatir);
 
         guncelbak = bakiye + yatir;
-        printf("Guncel bakiyen %d", guncelbak);
+        printf("New balance is %d $", guncelbak);
         break;
     case 'c':
-        printf("Ne kadar cekmek istersiniz:");
+        printf("How much you want to withdraw:");
         scanf("%d", &cek);
 
         guncelbak = bakiye - cek;
-        printf("Guncel bakiyen %d", guncelbak);
+        printf("New balance is %d $", guncelbak);
         break;
     default:
-        printf("Lütfen geçerli bir dişlem girin!");
+        printf("Invaild action!");
         break;
     }
     return 0;
